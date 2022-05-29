392946864d127d0aa0bc27402ccd96b42eff1cb1
marcelo garcia palma <marcelo.garcia@mercadolivre.com>
2022-05-29 12:49:46 -0300

    feat: adjust header and remove test application kotlin

--- front/booking-hotel-front/src/components/Header.js
+++ front/booking-hotel-front/src/components/Header.js
@@ -5,7 +5,7 @@ export const Header = (props) => (
     <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Temos os melhores hotéis de barrinha</h1>
+                    <h1 class="display-4 fw-bolder">Temos os melhores hotéis</h1>
                     <p class="lead fw-normal text-white-50 mb-0">Venha conhecer e viajar com a gente</p>
                 </div>
             </div>
